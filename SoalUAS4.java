609d02f73d4987fe892ce3e02ac62ac0acdbed10
jndan21 <118275197+Eschatos21xx@users.noreply.github.com>
2026-01-19 03:10:11 +0000

    commit

---
+++ SoalUAS4.java
@@ -0,0 +1,5 @@
+public class SoalUAS4 { public static void main(String[] args) {
+    int n=5;int i,j,k;for(i=0;i<n;i++){
+      for(j=2*(n-i);j>=0;j--){System.out.print(" ");}
+        for(k=0;k<=i;k++){System.out.print(k+" ");}System.out.println();}}}
+
